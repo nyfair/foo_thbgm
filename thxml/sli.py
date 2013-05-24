92eee04cc2eb224716ff680c898e6ff8c0d8178d
nyfair <nyfair2012@gmail.com>
2013-05-24 15:00:43 +0800

    oh garbage

--- thxml/sli.py
+++ thxml/sli.py
@@ -5,13 +5,10 @@ for file in glob.glob('*.sli'):
 		title = file[0 : file.rindex('.') - 4]
 		headlen = 0
 		looplen = 0
-		totallen = 0
 		for line in thbgm:
 			parts = line.split('=', 1)
 			if parts[0].lower() == 'loopstart':
 				headlen = int(parts[1])
 			elif parts[0].lower() == 'looplength':
 				looplen = int(parts[1])
-		if looplen == 0:
-			looplen = totallen - headlen
 		print('<bgm pos="0,%s,%s">%s</bgm>' % (headlen, looplen, title))
